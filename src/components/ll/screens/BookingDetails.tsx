f66b38f83a79b5c33de4529d76b13f9967f07cb8
taypye13 <taypye13@gmail.com>
2026-08-13 23:12:03 -0400

    Enable modify button and time change for LLSP bookings

--- src/components/ll/screens/BookingDetails.tsx
+++ src/components/ll/screens/BookingDetails.tsx
@@ -1,6 +1,6 @@
 import { use, useState } from 'react';
 
-import { Booking, isLLMP } from '@/api/itinerary';
+import { Booking, isLLMP, isLLSP } from '@/api/itinerary';
 import { Park } from '@/api/resort';
 import Alert from '@/components/Alert';
 import Button from '@/components/Button';
@@ -104,7 +104,7 @@ export default function BookingDetails({
         <ReturnTime
           {...booking}
           button={
-            isLLMP(booking) &&
+            (isLLMP(booking) || isLLSP(booking)) &&
             !unmodifiable &&
             ll.rules.timeSelect &&
             booking.modifiable && (
